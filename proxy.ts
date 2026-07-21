311c5e24e994646b500eab539b04a2962eb0f6de
aerkha <abdir971.ark@gmail.com>
2026-07-21 15:12:03 +0700

    revert zod

--- proxy.ts
+++ proxy.ts
@@ -57,7 +57,7 @@ function withRateLimitHeaders(res: NextResponse, opts: {
   return res;
 }
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
   // Belt-and-braces: matcher already restricts this, but be defensive.
